da0701f32838f19209842c9fc53422f66864d0b1
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-12-16 18:53:38 +0300

    rsps 3

---
+++ public_html/lib/module/sys-css.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="/lib/css/anim.css?v4"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/dark.css?v4"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/main.css?v4"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/debug.css?v4"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/scale.css?v4"/>
