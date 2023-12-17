ee7f551aaff81c6d28409f8f9681e8d7a1d27a53
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-12-17 15:44:53 +0300

    БизнесХау

--- lib/module/sys-css.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" type="text/css" href="/lib/css/anim.css?v4"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/dark.css?v4"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/main.css?v4"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/debug.css?v4"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/scale.css?v4"/>
